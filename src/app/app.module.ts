05fed5c5696a814df1586bfb583a07bc11feca6c
Ofori01 <128515949+Ofori01@users.noreply.github.com>
2024-08-27 15:28:38 +0000

    Added more components. Login, sign up Major changes to navbar <header component>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
     CommonModule,
     RouterOutlet,
     BrowserModule,
+    AppRoutingModule,
     SharedModule
   ],
   bootstrap: [AppComponent]
